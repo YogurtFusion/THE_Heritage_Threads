bc5b814c99018c5aa9d5626afb5973b5d4e69fde
YogurtFusion <cocaniket2@gmail.com>
2026-05-12 15:26:36 +0530

    improved users sidebar

--- sections/user/UserSidebar.jsx
+++ sections/user/UserSidebar.jsx
@@ -13,14 +13,14 @@ export const UserSidebar = () => {
   ];
 
   return (
-    <aside className="w-full md:w-48 lg:w-56 shrink-0">
+    <aside className="md:w-full  md:shrink-0">
       <h1 className="font-playfair text-3xl text-heading mb-10">My Account</h1>
       <nav className="flex flex-col space-y-6">
         {navItems.map((item) => (
           <button
             key={item.id}
             onClick={() => router.push(`/user?tab=${item.id}`)}
-            className={`flex justify-between items-center font-bold tracking-wider text-sm text-left transition-colors ${
+            className={`flex justify-between items-center font-bold tracking-wider text-sm text-left transition-colors cursor-pointer ${
               tab === item.id
                 ? "text-primary border-b border-primary pb-2"
                 : "text-heading hover:text-primary"
