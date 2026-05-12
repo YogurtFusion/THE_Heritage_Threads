cd89217144b607b61cf9513d815b8c5482de0ad4
YogurtFusion <cocaniket2@gmail.com>
2026-05-12 15:48:14 +0530

    improved users sidebar

--- sections/user/UserSidebar.jsx
+++ sections/user/UserSidebar.jsx
@@ -20,7 +20,7 @@ export const UserSidebar = () => {
           <button
             key={item.id}
             onClick={() => router.push(`/user?tab=${item.id}`)}
-            className={`flex justify-between items-center font-bold tracking-wider text-sm text-left transition-colors cursor-pointer ${
+            className={`flex justify-between items-center font-bold tracking-wider text-sm text-left transition-colors cursor-pointer w-fit ${
               tab === item.id
                 ? "text-primary border-b border-primary pb-2"
                 : "text-heading hover:text-primary"
