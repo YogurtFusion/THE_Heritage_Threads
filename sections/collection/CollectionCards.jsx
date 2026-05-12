bd74cfc22bd37a536bbfec87edf7e23007942212
YogurtFusion <cocaniket2@gmail.com>
2026-05-12 15:55:19 +0530

    improved collection

--- sections/collection/CollectionCards.jsx
+++ sections/collection/CollectionCards.jsx
@@ -22,7 +22,7 @@ export const CollectionCards = ({ _id, id, img, images, title, name, price, inde
   };
 
   return (
-    <article className="group flex flex-col h-full">
+    <article className="group flex flex-col ">
       <Link
         href={isApiProduct ? `/product/${productId}` : "/preview"}
         onMouseEnter={() => setHovered(true)}
@@ -62,7 +62,7 @@ export const CollectionCards = ({ _id, id, img, images, title, name, price, inde
           {!mainImage && <div className="w-full h-full bg-section" />}
         </div>
       </Link>
-      <h3 className="font-inter text-xl text-heading mb-1">{displayTitle}</h3>
+      <h3 className="font-inter text-base font-bold  text-heading mb-1 line-clamp-2">{displayTitle}</h3>
       <p className="text-sm text-muted-text mb-4">₹{price}</p>
       <AddCartBtn product={product} />
     </article>
